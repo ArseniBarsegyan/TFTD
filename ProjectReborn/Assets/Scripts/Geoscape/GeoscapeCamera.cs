eea3a61d1206f4f7fd54e3fb16840ec48f0bd4ec
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2019-07-21 22:43:58 +0300

    Implemented camera movement to clicked position.

--- ProjectReborn/Assets/Scripts/Geoscape/GeoscapeCamera.cs
+++ ProjectReborn/Assets/Scripts/Geoscape/GeoscapeCamera.cs
@@ -1,17 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class GeoscapeCamera : MonoBehaviour
 {
-    private float _zoomSpeed = 2.0f;
     private float _rotateSpeed = 4.0f;
-    private float _minDistance = 6.0f;
-    private float _maxDistance = 12.0f;
     [SerializeField] private GameObject geoscape;
 
     private float _rotX;
     private float _rotY;
     private Vector3 _offset;
+    private float _cameraDistance = 5.0f;
 
     void Start()
     {
@@ -20,46 +19,18 @@ public class GeoscapeCamera : MonoBehaviour
 
     void Update()
     {
-        // Zoom in
-        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        //transform.RotateAround(geoscape.transform.position, Vector3.up, 45 * Time.deltaTime);
+        //transform.RotateAround(geoscape.transform.position, Vector3.right, 45 * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(1))
         {
-            Vector3 cameraPosition = transform.position;
-            Vector3 geoscapePosition = geoscape.transform.position;
-            float distance = Vector3.Distance(cameraPosition, geoscapePosition);
-            if (distance > _minDistance)
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                Vector3 targetDestination = transform.position + transform.forward * _zoomSpeed;
-                StartCoroutine(ZoomSmoothly(targetDestination));
+                StartCoroutine(MoveCamera(hit.point));
             }
         }
-
-        // Zoom out
-        if (Input.GetAxis("Mouse ScrollWheel") < 0)
-        {
-            Vector3 cameraPosition = transform.position;
-            Vector3 geoscapePosition = geoscape.transform.position;
-            float distance = Vector3.Distance(cameraPosition, geoscapePosition);
-
-            if (distance < _maxDistance)
-            {
-                Vector3 targetDestination = transform.position - transform.forward * _zoomSpeed;
-
-                StartCoroutine(ZoomSmoothly(targetDestination));
-            }
-        }
-    }
-
-    private IEnumerator ZoomSmoothly(Vector3 destination)
-    {
-        float t = 0f;
-        while (t < 0.5f)
-        {
-            t += Time.deltaTime;
-            transform.position = Vector3.Lerp(transform.position, destination, Mathf.SmoothStep(0f, 0.5f, t));
-            transform.LookAt(geoscape.transform);
-            yield return null;
-        }
-    }
+    }   
 
     void LateUpdate()
     {
@@ -88,47 +59,37 @@ public class GeoscapeCamera : MonoBehaviour
             }
 
             Quaternion rotation = Quaternion.Euler(_rotX, _rotY, 0);
-            Vector3 targetPosition = geoscape.transform.position - (rotation * _offset);
-            StartCoroutine(Rotate_Routine(targetPosition));
-        }
+            Vector3 targetPosition = geoscape.transform.position - rotation * _offset;
 
-        if (Input.GetMouseButtonDown(1))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                Quaternion targetRotation = Quaternion.LookRotation(transform.position, hit.transform.forward);
-
-                Vector3 newCameraPosition = hit.point + (targetRotation * _offset * 0.4f);
-                StartCoroutine(Move_Routine(hit.transform.right, newCameraPosition));
-            }
+            transform.position = targetPosition;
+            transform.LookAt(geoscape.transform);
         }
     }
 
-    private IEnumerator Move_Routine(Vector3 observableTarget, Vector3 targetPosition)
+    // Create camera observe point and move move camera to that point smoothly
+    private IEnumerator MoveCamera(Vector3 targetPoint)
     {
+        GameObject observePoint = new GameObject();
+        observePoint.transform.position = targetPoint;
+        observePoint.transform.LookAt(geoscape.transform);
+        observePoint.transform.position -= observePoint.transform.forward * _cameraDistance;
+        Destroy(observePoint);
+
+        Vector3 observePosition = observePoint.transform.position;
+
         float t = 0f;
         while (t < 1f)
         {
             t += Time.deltaTime;
-            transform.position = Vector3.Lerp(transform.position, targetPosition, Mathf.SmoothStep(0f, 1f, t));
-            transform.LookAt(observableTarget);
-            yield return null;
-        }
-    }
-
-    private IEnumerator Rotate_Routine(Vector3 targetPosition)
-    {
-        float t = 0f;
-        while (t < 1f)
-        {
-            t += Time.deltaTime;
-            transform.position = Vector3.Lerp(transform.position, targetPosition, Mathf.SmoothStep(0f, 1f, t));
+            transform.position = Vector3.Lerp(transform.position, observePosition, 
+                Mathf.SmoothStep(0f, 1f, t));
             transform.LookAt(geoscape.transform);
             yield return null;
         }
-    }
 
+        yield return null;
+    }
+    
     // TODO: Later replace with Alien Sub spawn
     private IEnumerator EmptyObject(Vector3 pos)
     {
